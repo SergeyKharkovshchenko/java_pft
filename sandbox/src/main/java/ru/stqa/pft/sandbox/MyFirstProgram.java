2ec02d93b8f45fab56d1cfcf613a71182bbc081f
SergeyKharkovshchenko <kharkovchenko@gmail.com>
2016-04-12 20:36:24 +0300

    Добавлен класс Point и метод этого класса distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,24 +13,14 @@ public static void main (String[] args) {
     Rectangle r = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " = " + r.area ());
 
-    Point p1 = new Point ();
-    Point p2 = new Point ();
-    p1.x = 2;
-    p1.y = 5;
-    p2.x = 7;
-    p2.y = 10;
-    System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y +") и p2(" + p2.x + ";" + p2.y + ") = " + distance(p1,p2));
-
+    Point p1 = new Point (2,5);
+    Point p2 = new Point (7,10);
+        System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y +") и p2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
+        System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y +") и p2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p1,p2));
 	}
 
     public static void hello( String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
-    }
-
-
 
 }
