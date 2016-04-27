7aee37f473832c5d1f5c5bbb99f45e91c66a7ad2
SergeyKharkovshchenko <kharkovchenko@gmail.com>
2016-04-27 12:41:48 +0300

    Реализованы проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -46,4 +46,15 @@ public class GroupHelper extends HelperBase {
   public void submitGroupModifocation() {
     click(By.name("update"));
   }
+
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereAGroup() {
+    return isElementPresent(By.xpath("//div[@id='content']/form/span[1]/input"));
+  }
 }
