76c71b4319a7b54037fc73cb81cb6d0354da4f91
SergeyKharkovshchenko <kharkovchenko@gmail.com>
2016-05-04 16:32:57 +0300

    Добавлена анонимная функция для сортировки списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -21,12 +21,7 @@ public class GroupData {
     this.footer = footer;
   }
 
-  public GroupData(String name, String header, String footer) {
-    this.id = 0;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
+
 
   public String getName() {
     return name;
@@ -48,8 +43,6 @@ public class GroupData {
             '}';
   }
 
-
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -57,15 +50,20 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
 
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+    return name != null ? name.hashCode() : 0;
   }
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
 }
