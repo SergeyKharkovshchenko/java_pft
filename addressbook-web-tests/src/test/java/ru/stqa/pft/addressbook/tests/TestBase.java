d9fde01fd089f8ea7bc3b46c479f72fb21241e00
SergeyKharkovshchenko <kharkovchenko@gmail.com>
2016-06-16 16:39:21 +0300

    Добавлена генерация отчетов при помощи Allure

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.SystemClock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -26,6 +24,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Created by Sergey on 20.04.2016.
  */
+
+@Listeners (MyTestListener.class)
+
 public class TestBase {
 
   Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -34,8 +35,9 @@ public class TestBase {
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
-  public void setUp() throws Exception {
+  public void setUp(ITestContext context) throws Exception {
     app.init();
+    context.setAttribute("app", app);
   }
 
   @AfterSuite (alwaysRun = true)
